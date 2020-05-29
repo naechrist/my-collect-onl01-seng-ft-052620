5af260fbc81c6c4b166c08e3ea7646510d283436
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-29 23:12:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array) 
+ 
+new_array = []
+index = 0 
+
+while index < array.length 
+new_array << yield(array[index])
+index += 1 
+
+end
+new_array
+end
 
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
